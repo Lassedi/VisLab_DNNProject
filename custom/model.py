083d66446ecdd4d0a0eea4bb74e8be4ae7f80d63
lassedi <lassedi@outlook.de>
2023-05-20 13:40:25 +0200

    "Created Training loop & function to create folder structure"

--- custom/model.py
+++ custom/model.py
@@ -5,20 +5,20 @@ class FreeConvNetwork(nn.Module):
     def __init__(self):
         super().__init__() # call upon parent class constructor ie their attributes and methods 
         self.freeConvStack = nn.Sequential( #define the order in which data passes through net
-            LocallyConnected2d(3, out_channels=32, output_size=(54,44), kernel_size=6, stride=4, bias=True),
+            LocallyConnected2d(3, out_channels=8, output_size=(54,44), kernel_size=6, stride=4, bias=True),
             # output_size = ((input size(width/height of img) - kernel_size + 2xpadding) / stride) + 1(for bias)
             # weights = (kernel_size*kernel_size*out_channels+1(for bias))*output_size*output_size
             nn.ReLU(),
             
-            LocallyConnected2d(32, 64, (13,10), 6, 4,True), # here input size = output size previous layer not out_channels
+            LocallyConnected2d(8, 16, (13,10), 6, 4,True), # here input size = output size previous layer not out_channels
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2),
             
             nn.Flatten(),
             #nn.Linear() #how to calculate input size for this layer?
-            nn.LazyLinear(128),
+            nn.Linear(16*6*5, 300), # input size = output_channels*output_size/2 of last Local layer because of maxpool2d kernel size 2
             nn.ReLU(),
-            nn.Linear(128, 10)
+            nn.Linear(300, 200)
         )
 
     def forward(self, x):
---
+++ utils/create_train_test_folder_structure.py
@@ -0,0 +1,38 @@
+import shutil
+import os
+import pandas as pd
+import numpy as np
+def create_train_test_folder_structure(n_classes, path):
+    
+    # read in the list of celebreties left in the folder
+    if not os.path.isfile("{}/Data/identity_CelebA_updated.txt".format(path)):
+        identity_keys = pd.read_csv("{}/Data/identity_CelebA.txt".format(path), sep=" ", header=None, names = ["file_name", "pict_id"])
+    else:
+        identity_keys = pd.read_csv("{}/Data/identity_CelebA_updated.txt".format(path), sep = " ") 
+    identity_key_updated = identity_keys
+    
+    # create train & test folders by moving images in to their respective classes
+    for n_id in range(n_classes):
+        x = identity_keys.pict_id[n_id]
+        identity_x = identity_keys[identity_keys.pict_id == x].reset_index(drop = True)
+
+        
+        for n_img in range(len(identity_x)):
+            id_x_file = identity_x.iloc[n_img,0]
+
+            path_original = "{}/Data/img_align_celeba/img_align_celeba/{}".format(path,id_x_file)
+            path_target = "{}/Data/img_align_celeba/class_data/{}/".format(path, x)
+            
+            # make sure target directory exists
+            if not os.path.exists(path_target):
+                os.makedirs(path_target)
+            try:
+                shutil.move(path_original, path_target)
+            except:
+                FileNotFoundError
+                print("File Not Found: {}".format(id_x_file))
+                pass
+        identity_key_updated = identity_key_updated[identity_key_updated.pict_id != x]
+
+    # save an updated list of celebreties left in the original folder
+    identity_key_updated.to_csv("./Data/identity_CelebA_updated.txt", sep = " ", index = False)
