d6e4c486e7fbd7857c2d64511b248a48654d7a85
lassedi <lassedi@outlook.de>
2023-06-12 23:26:08 +0200

    Testing FCV and ConvNet on MNIST - ConvNet outperforms

---
+++ custom/model_conv.py
@@ -0,0 +1,16 @@
+from torch import nn 
+
+class ConvNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.sequential_stack = nn.Sequential(
+            nn.Conv2d(1, 32, 3, stride=2, bias=True),
+            nn.Conv2d(32,64, 3, 2, bias= True),
+            nn.Conv2d(64,128, 3, 1, bias=True),
+            nn.Flatten(),
+            nn.Linear(2048, 10)
+        )
+
+    def forward(self, x):
+        output = self.sequential_stack(x)
+        return output
